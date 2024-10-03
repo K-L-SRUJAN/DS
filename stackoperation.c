34331c740c071e21f57ce4ef10dfd0d6985fea7d
K L SRUJAN <klsrujan.cs24@bmsce.ac.in>
2024-10-03 12:52:55 +0530

    Create stackoperation.c

---
+++ stackoperation.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define size 5
+
+int arra[size];
+int top=-1;
+int x;
+int main(){
+    int v=0;
+    while(v==0){
+        printf("\nenter 1 for push, 2 for pop, 3 for display , any other number to exit: ");
+    int d;
+    scanf("%d",&d);
+    if(d==1){
+        if(top>=size){
+            printf("stack full\n");
+            printf("");
+        }
+        top=top+1;
+        printf("ENTER THE NUMBER :");
+        printf("");
+        scanf("%d",&x);
+        arra[top]=x;
+        printf("new element added :%d\n",arra[top]);
+        printf("");
+    }
+    else if(d==2){
+        if(top==-1){
+            printf("empty stack");
+            printf("");
+        }
+        printf("the deleted element is :%d\n",arra[top]);
+        printf("");
+        top=top-1;
+        
+    }
+    else if(d==3){
+        for(int i=0;i<size;i++){
+                break;
+            }
+            printf("%d",arra[i]);
+            printf("");
+        }
+    }
+    else{
+        break;
+    }
+    }
+}
